54b19452c363d80bf38166ee6309ed6cfe4a6471
francesco.porcu.dev <francesco.porcu.dev@outlook.it>
2021-01-21 17:47:21 +0100

    Modifica Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Text, View } from "react-native";
+
+import React from "react"
+import { View } from "react-native"
 
 import styles from "./styles";
+import { Styles } from '../../styles'
 
 const Home = (props) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>Home</Text>
-      </View>
-    </View>
-  );
-};
+    return (
+        <View style={Styles.Commons.container}>
+            <View style={Styles.Commons.content}>
+                
+            </View>
+        </View>
+    )
+}
 
 export default Home
