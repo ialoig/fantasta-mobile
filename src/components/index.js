167b5f50ed714bbd2f47a4c1f606ce5fb354a847
ialoig <ialoig@protonmail.com>
2021-02-12 18:04:24 +0100

    Added PrevButton & Navigator

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,5 @@ export { default as Button } from './Button/ButtonContainer'
 export { default as Logo } from './Logo/LogoContainer'
 
 export { default as Card } from './Card/CardContainer'
+
+export { default as PrevButton } from './PrevButton/PrevButtonContainer'
