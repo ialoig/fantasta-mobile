2c97470f5a3c4e4afd2cc479fb657f3a5d055b62
ialoig <giovannilai86@gmail.com>
2021-02-26 17:44:42 +0100

    #22 #47 Fixed style on Login&Register page

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -2,5 +2,8 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     logo: {
+        flex: 1,
+        resizeMode: "contain",
+        alignSelf: "center"
     }
 });
