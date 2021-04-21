aab59bca97384c7cbde25341a48d919ceb1d7200
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-04-22 00:04:29 +0200

    wip

--- src/containers/SplashScreen/SplashScreenContainer.js
+++ src/containers/SplashScreen/SplashScreenContainer.js
@@ -10,92 +10,48 @@ import Constants from 'expo-constants'
 
 export class SplashScreenContainer extends React.Component {
 
-	state = {
-		redirectData: null,
-	};
+	deepLinkRedirection = async (url) => {
 
-	_checkDeepLink = async () => {
-
-		console.log(`_checkDeepLink`)
-
+		// Check device OS
 		if (Constants.platform.ios) {
 			console.log("running on iOS")
 		} else {
 			console.log("running on Android")
 		}
 
-		const url = await Linking.getInitialURL()
-		console.log(`url: ${url}`)
-
 		let { path, queryParams } = Linking.parse(url)
+		console.log(`url: ${url}`)
 		console.log(`path: ${path}`)
 		console.log(`queryParams: ${JSON.stringify(queryParams, null, 2)}`)
+
 		if (path != "") {
-			console.log(`DEEP LINK`)
-			// this._handleRedirect(path, queryParams);
-			this._handleRedirect(url);
+			console.log(`App started via DeepLink`)
+			this.setState({ deepLink: true })
+			this.handleRedirect(url)
 		}
 		else {
-			console.log(`NO DEEP LINK`)
+			this.setState({ deepLink: false })
+			console.log(`App started normally`)
 		}
 	}
 
-	_handleRedirect = (url) => {
+	handleRedirect = (url) => {
 		let { path, queryParams } = Linking.parse(url)
-
-		console.log(`******************** _handleRedirect`)
-		console.log(`path: ${path}`)
-		console.log(`queryParams: ${JSON.stringify(queryParams, null, 2)}`)
-
-		// this.setState({ redirectData: data });
 		this.props.navigation.navigate(path, queryParams)
-	};
-
-	_addLinkingListener = () => {
-		console.log("********** _addLinkingListener")
-		// Linking.addEventListener("url", this._handleRedirect);
-		Linking.addEventListener('url', event => {
-			console.log(`event url: ${event.url}`)
-			this._handleRedirect(event.url);
-		});
-	};
-
-	_removeLinkingListener = () => {
-		console.log("********** _removeLinkingListener")
-		Linking.removeEventListener("url", this._handleRedirect);
 	};
 
 	constructor(props) {
 		super(props);
 
-		// listen for new url events coming from Expo
-		this._addLinkingListener();
+		this.state = {
+			deepLink: false
+		}
+
+		// listen for "url" events coming from Expo
+		Linking.addEventListener("url", (event) => this.handleRedirect(event.url));
 	}
 
-	// urlRedirect(url) {
-	// 	console.log(`urlRedirect: url=${url}`);
-	// 	if (!url) return;
-	// 	// parse and redirect to new url
-	// 	let { path, queryParams } = Linking.parse(url);
-	// 	console.log(`Linked to app with path: ${path} and data: ${JSON.stringify(queryParams)}`);
-	// 	this.props.navigation.navigate(path, queryParams)
-	// 	// this.props.navigation.navigate(path)
-	// }
-
-	// constructor(props) {
-	// 	super(props);
-
-	// 	// listen for new url events coming from Expo
-	// 	Linking.addEventListener('url', event => {
-	// 		console.log(`event url: ${event.url}`)
-	// 		this.urlRedirect(event.url);
-	// 	});
-	// }
-
 	async componentDidMount() {
-
-		// Check if app was opened from a deep link
-		this._checkDeepLink()
 
 		try {
 			await Init()
@@ -104,23 +60,29 @@ export class SplashScreenContainer extends React.Component {
 			//TODO: come vogliamo gestire l'errore???
 		}
 
-		let token = Token.Get()
-		if (token) {
-			try {
-				let res = await Auth.Authenticate(token)
-				this.props.navigation.navigate(routes.HOME)
+		// Check whether the app was open via a deepLink
+		this.deepLinkRedirection(await Linking.getInitialURL())
+
+		// App started normally
+		if (!this.state.deepLink) {
+			let token = Token.Get()
+			if (token) {
+				try {
+					let res = await Auth.Authenticate(token)
+					this.props.navigation.navigate(routes.HOME)
+				}
+				catch (error) {
+					this.props.navigation.navigate(routes.LOGIN)
+				}
 			}
-			catch (error) {
+			else {
 				this.props.navigation.navigate(routes.LOGIN)
 			}
-		}
-		else {
-			this.props.navigation.navigate(routes.LOGIN)
 		}
 	}
 
 	async componentWillUnmount() {
-		this._removeLinkingListener
+		Linking.removeEventListener("url", this._handleRedirect);
 	}
 
 	render() {
@@ -130,7 +92,6 @@ export class SplashScreenContainer extends React.Component {
 		)
 	}
 }
-
 
 SplashScreenContainer.propTypes = {
 	navigation: PropTypes.object.isRequired
