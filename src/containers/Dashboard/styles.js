b55b5800c3989aa06f417be86f8e15abaa469b01
ialoig <ialoig@protonmail.com>
2021-07-30 20:54:12 +0200

    Chart first imp

--- src/containers/Dashboard/styles.js
+++ src/containers/Dashboard/styles.js
@@ -2,8 +2,21 @@
 import { StyleSheet } from "react-native"
 import colors from "../../styles/colors"
 import { deviceScreenHeight, deviceScreenWidth } from "../../utils/deviceUtils"
+import { dynamicHeight, dynamicWidth } from "../../utils/pixelResolver"
 
 export default StyleSheet.create({
+	chart: {
+		...StyleSheet.absoluteFillObject,
+		alignContent: "center",
+		backgroundColor: colors.greyLight,
+		borderRadius: 24,
+		height: dynamicHeight(327, 180),
+		justifyContent: "center",
+		marginHorizontal: 0,
+		marginTop: 12,
+		position: "relative",
+		width: dynamicWidth(327)
+	},
 	container: {
 		...StyleSheet.absoluteFillObject,
 		alignContent: "center",
@@ -13,5 +26,5 @@ export default StyleSheet.create({
 	},
 	team: {
 		height: deviceScreenHeight - 150,
-	}
+	},
 })
