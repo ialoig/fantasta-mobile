7322c211ce45bc7ff3499e9c1473a8268ceca269
ialoig <ialoig@protonmail.com>
2021-08-13 15:34:39 +0200

    reordered comps

---
+++ src/components/Players/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+
+export default StyleSheet.create({
+	badges: {
+		flexDirection: "row",
+		paddingTop: 8,
+	},
+	list: {
+		paddingBottom: 120,
+		paddingTop: 20
+	},
+	playerContainer: {
+		flex: 1,
+	}
+})
