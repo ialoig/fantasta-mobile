b45b377589e395a6925210a1128c6f4011340412
ialoig <ialoig@protonmail.com>
2021-08-13 18:06:12 +0200

    Fixed logo

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -1,8 +1,31 @@
 import { StyleSheet } from "react-native"
+import { colors } from "../../styles"
 
 export default StyleSheet.create({
 	center: {
 		alignSelf: "center",
-		padding: 12
+	},
+	container: {
+		...StyleSheet.absoluteFillObject,
+		alignItems: "center",
+		justifyContent: "center",
+		paddingVertical: 24,
+		position: "relative",
+	},
+	text: {
+		color: colors.text,
+		fontFamily: "PoppinsMedium",
+		fontSize: 35,
+		fontStyle: "normal",
+		fontWeight: "500",
+		letterSpacing: 0,
+		textAlign: "center"
+	},
+	title:{
+		alignItems: "center",
+		justifyContent: "center",
+		paddingHorizontal: 12,
+		paddingVertical: 8,
+		position: "relative"
 	}
 })
