cf15c6a1a9f5fd1f8d02d47466ffda12fddaf0e6
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-10-03 00:23:51 +0200

    validate fields before switching page in CreateLeague

--- src/containers/Create/CreateContainer.js
+++ src/containers/Create/CreateContainer.js
@@ -42,6 +42,7 @@ export class CreateContainer extends React.Component {
 		}
 	}
 
+	// called when changing any of the fields in the slide
 	onChange(id, value) {
 
 		let settings = {
@@ -53,41 +54,89 @@ export class CreateContainer extends React.Component {
 		})
 	}
 
-	async onDone() {
+	validate_createLeague() {
 		if (!this.state.settings[FIELDS_ID.leagueNameId]) {
-			Error.showAlert("field_error", "missing_league_name")
+			Error.showAlert("field_error", "missing_league_name") // todo: no alert but show message
+			return false
 		}
-		else if (!this.state.settings[FIELDS_ID.passwordId]) {
+		if (!this.state.settings[FIELDS_ID.passwordId]) {
 			Error.showAlert("field_error", "missing_password")
+			return false
 		}
-		else if (!this.state.settings[FIELDS_ID.teamnameId]) {
-			Error.showAlert("field_error", "missing_team_name")
-		}
-		else if (this.state.settings[FIELDS_ID.participantsId] < 2) {
+		if (this.state.settings[FIELDS_ID.participantsId] < 2) {
 			Error.showAlert("field_error", "participants_error")
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.goalskeepersId] < 1) {
-			Error.showAlert("field_error", "goalskeepers_error")
-		}
-		else if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.defendersId] < 3) {
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.defendersId] < 3) {
 			Error.showAlert("field_error", "defenders_error")
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.midfieldersId] < 3) {
+		return true
+	}
+
+	validate_teamSettings() {
+		if (this.state.settings[FIELDS_ID.goalskeepersId] < 1) {
+			Error.showAlert("field_error", "goalskeepers_error")
+			return false
+		}
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.defendersId] < 3) {
+			Error.showAlert("field_error", "defenders_error")
+			return false
+		}
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.midfieldersId] < 3) {
 			Error.showAlert("field_error", "midfielders_error")
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.strikersId] < 1) {
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.strikersId] < 1) {
 			Error.showAlert("field_error", "forwarders_error")
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.defendersId] + this.state.settings[FIELDS_ID.midfieldersId] + this.state.settings[FIELDS_ID.strikersId] < 10) {
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.CLASSIC && this.state.settings[FIELDS_ID.defendersId] + this.state.settings[FIELDS_ID.midfieldersId] + this.state.settings[FIELDS_ID.strikersId] < 10) {
 			Error.showAlert("field_error", "players_error") // todo: maybe a better error?
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.MANTRA && this.state.settings[FIELDS_ID.playersId] < 10) {
+		if (this.state.settings[FIELDS_ID.tipologyId] == TIPOLOGY.MANTRA && this.state.settings[FIELDS_ID.playersId] < 10) {
 			Error.showAlert("field_error", "players_error")
+			return false
 		}
-		else if (this.state.settings[FIELDS_ID.countdownId] < 3) {
+		return true
+	 }
+
+	validate_auctionSettings() { 
+		if (this.state.settings[FIELDS_ID.countdownId] < 3) {
 			Error.showAlert("field_error", "countdown_error")
+			return false
 		}
-		else {
+		return true
+	}
+
+	validate_createTeam() { 
+		if (!this.state.settings[FIELDS_ID.teamnameId]) {
+			Error.showAlert("field_error", "missing_team_name")
+			return false
+		}
+		return true
+	}
+
+	validate_page(page_index) {
+		let validate_page_result = true
+		switch (page_index) {
+			case 0:
+				return this.validate_createLeague()
+			case 1:
+				return this.validate_teamSettings()
+			case 2:
+				return this.validate_auctionSettings()
+			case 3:
+				return this.validate_createTeam()
+			default:
+				console.log(`page_index: ${page_index}. No validation defined for page "${pages[page_index].title}"`)
+		}
+		return validate_page_result
+	}
+
+	async onDone() {
+		if (this.validate_page(pages.length - 1)){
 			try {
 				await Leagues.Create(this.state.settings)
 				this.props.navigation.navigate(routes.BOTTOMTABNAVIGATOR)
@@ -117,6 +166,7 @@ export class CreateContainer extends React.Component {
 				auctionType={AUCTION_TYPE}
 				startingPrice={STARTING_PRICE}
 				pages={pages}
+				validate_page={this.validate_page.bind(this)}
 				settings={this.state.settings}
 				onChange={this.onChange.bind(this)}
 				onDone={this.onDone.bind(this)}
