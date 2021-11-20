61f51d8fe47944188c1efbc24656a23c6b37e3b4
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-11-21 00:27:17 +0100

    join room and handle message in Dashboard page

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -6,6 +6,7 @@ import Dashboard from "../containers/Dashboard/Dashboard"
 import Market from "../containers/Market/Market"
 import PlayersContainer from "../containers/Players/PlayersContainer"
 import Team from "../containers/Team/Team"
+import { SocketManager } from "../services/socket"
 import { colors } from "../styles"
 import { bottomTabShadow, bottomTabStyle } from "./bottomTabNavigatorConfig"
 import routes from "./routesNames"
@@ -13,74 +14,77 @@ import routes from "./routesNames"
 
 
 const Tab = createBottomTabNavigator()
+const socket = SocketManager.getSocketInstance()
 
 function BottomTabNavigator() {
 	return (
-		<Tab.Navigator 
-			initialRouteName={routes.DASHBOARD}
-			backBehavior="none"
-			tabBarOptions={{
-				showLabel: false,
-				style: {
-					...bottomTabStyle,
-					...bottomTabShadow
-				},
-				keyboardHidesTabBar: true
-			}}
-		>
-			<Tab.Screen 
-				name={routes.DASHBOARD}
-				component={Dashboard}
-				options={{ 
-					// eslint-disable-next-line react/display-name
-					tabBarIcon: ({ focused }) => {
-						return <Icon
-							name="dashboard_tab" 
-							primary={focused ? colors.primary : colors.grey} 
-							secondary={focused ? colors.secondary : colors.white} />
-					}
+		<socket.SocketContext.Provider value={socket.ioClient}>
+			<Tab.Navigator 
+				initialRouteName={routes.DASHBOARD}
+				backBehavior="none"
+				tabBarOptions={{
+					showLabel: false,
+					style: {
+						...bottomTabStyle,
+						...bottomTabShadow
+					},
+					keyboardHidesTabBar: true
 				}}
-			/>
-			<Tab.Screen 
-				name={routes.TEAM}
-				component={Team}
-				options={{ 
+			>
+				<Tab.Screen 
+					name={routes.DASHBOARD}
+					component={Dashboard}
+					options={{ 
 					// eslint-disable-next-line react/display-name
-					tabBarIcon: ({ focused }) => {
-						return <Icon
-							name="teams_tab" 
-							primary={focused ? colors.primary : colors.grey} 
-							secondary={focused ? colors.secondary : colors.white} />
-					}
-				}}
-			/>
-			<Tab.Screen 
-				name={routes.MARKET}
-				component={Market}
-				options={{ 
+						tabBarIcon: ({ focused }) => {
+							return <Icon
+								name="dashboard_tab" 
+								primary={focused ? colors.primary : colors.grey} 
+								secondary={focused ? colors.secondary : colors.white} />
+						}
+					}}
+				/>
+				<Tab.Screen 
+					name={routes.TEAM}
+					component={Team}
+					options={{ 
 					// eslint-disable-next-line react/display-name
-					tabBarIcon: ({ focused }) => {
-						return <Icon
-							name="market_tab" 
-							primary={focused ? colors.primary : colors.grey} 
-							secondary={focused ? colors.secondary : colors.white} />
-					}
-				}}
-			/>
-			<Tab.Screen 
-				name={routes.PLAYERS}
-				component={PlayersContainer}
-				options={{ 
+						tabBarIcon: ({ focused }) => {
+							return <Icon
+								name="teams_tab" 
+								primary={focused ? colors.primary : colors.grey} 
+								secondary={focused ? colors.secondary : colors.white} />
+						}
+					}}
+				/>
+				<Tab.Screen 
+					name={routes.MARKET}
+					component={Market}
+					options={{ 
 					// eslint-disable-next-line react/display-name
-					tabBarIcon: ({ focused }) => {
-						return <Icon
-							name="players_tab" 
-							primary={focused ? colors.primary : colors.grey} 
-							secondary={focused ? colors.secondary : colors.white} />
-					}
-				}}
-			/>
-		</Tab.Navigator>
+						tabBarIcon: ({ focused }) => {
+							return <Icon
+								name="market_tab" 
+								primary={focused ? colors.primary : colors.grey} 
+								secondary={focused ? colors.secondary : colors.white} />
+						}
+					}}
+				/>
+				<Tab.Screen 
+					name={routes.PLAYERS}
+					component={PlayersContainer}
+					options={{ 
+					// eslint-disable-next-line react/display-name
+						tabBarIcon: ({ focused }) => {
+							return <Icon
+								name="players_tab" 
+								primary={focused ? colors.primary : colors.grey} 
+								secondary={focused ? colors.secondary : colors.white} />
+						}
+					}}
+				/>
+			</Tab.Navigator>
+		</socket.SocketContext.Provider>
 	)
 }
 
