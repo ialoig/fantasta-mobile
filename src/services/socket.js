227982335f49e207c2852d00ee344c833b975ea4
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-11-21 14:49:54 +0100

    restructure events

--- src/services/socket.js
+++ src/services/socket.js
@@ -8,6 +8,55 @@ const EVENT_TYPE = {
 	// FootballPlayer
 	SERVER_FOOTBALL_PLAYER_LIST_UPDATE:0,
   
+	// Events sent by the mobile app
+	CLIENT: {
+	  
+	  LEAGUE: {
+		USER_NEW: 0,
+		USER_EXIT: 0,
+		USER_ONLINE:0,
+		USER_OFFLINE:0
+	  },
+  
+	  MARKET: {
+		OPEN: 0,
+		USER_ONLINE: 0,
+		START: 0,
+		PLAYER_SELECTED: 0,
+		BET: 0,
+		WIN: 0,
+		PAUSE: 0,
+		CLOSE: 0,
+		USER_OFFLINE: 0,
+	  }
+  
+	},
+	
+	// Events sent by the server
+	SERVER: {
+  
+	  LEAGUE: {
+		USER_NEW: 0,
+		USER_EXIT: 0,
+		USER_ONLINE: 0,
+		USER_OFFLINE: 0
+	  },
+  
+	  MARKET: {
+		OPEN: 0,
+		USER_ONLINE: 0,
+		START: 0,
+		SEARCH: 0,
+		PLAYER_SELECTED: 0,
+		BET: 0,
+		WIN: 0,
+		PAUSE: 0,
+		CLOSE: 0,
+		USER_OFFLINE: 0,
+	  }
+	  
+	},
+  /*
 	// League
 	CLIENT_LEAGUE_JOIN: 0,
 	SERVER_LEAGUE_JOIN: 0,
@@ -36,7 +85,8 @@ const EVENT_TYPE = {
 	SERVER_MARKET_CLOSE: 0,
 	CLIENT_MARKET_LEFT: 0,
 	SERVER_MARKET_LEFT: 0,
-}
+	*/
+  }
 
 class Socket {
 
@@ -52,7 +102,7 @@ class Socket {
 		this.league = leagueName
 		this.player = User.get().username
 		console.log(`[Socket] joinRoom league=${this.league} player=${this.player}`)
-		this.ioClient.emit(EVENT_TYPE.CLIENT_LEAGUE_JOIN, { room: this.league, player: this.player }, (response) => {
+		this.ioClient.emit(EVENT_TYPE.CLIENT.LEAGUE.USER_ONLINE, { room: this.league, player: this.player }, (response) => {
 			console.log(`response.status: ${response.status}`)
 			console.log(`response.error: ${JSON.stringify(response.error, null, 2)}`)
 		})
@@ -63,11 +113,11 @@ class Socket {
 		const { event_type, data } = payload
 	
 		switch (event_type) {
-		case EVENT_TYPE.SERVER_LEAGUE_JOIN:
+		case EVENT_TYPE.SERVER.LEAGUE.USER_ONLINE:
 			console.log(`[Socket] user joined room ${this.league}. players online: ${data}`)
 			break
 	
-		case EVENT_TYPE.SERVER_LEAGUE_LEFT:
+		case EVENT_TYPE.SERVER.LEAGUE.USER_OFFLINE:
 			console.log(`[Socket] user left room ${this.league}. players online: ${data}`)
 			break
 	
