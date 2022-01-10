89332387e594e070d40c5201c7fd559e2e146c72
Filippo Mascolo <mascolo.filippo@gmail.com>
2022-01-11 00:44:13 +0100

    wip

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -29,77 +29,99 @@ function BottomTabNavigator() {
 
 	useEffect(() => {
 
-		// League events
+		// ------- League events -------
+		// New user joined the League (not used)
+		// use case: force reload league data to see new user in Team page
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.LEAGUE.USER_NEW, (payload) => {
 			console.log(`[Socket] user joined league room "${socket.league_room}" (it's a NEW user). users online: ${payload}`)
 			//TODO: fetch league data again
 			setOnlinePlayersLeague(payload)
 		})
 	
+		// Existing user joined the League (not used)
+		// use case: tbd
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.LEAGUE.USER_ONLINE, (payload) => {
 			console.log(`[Socket] user joined league room "${socket.league_room}". users online: ${payload}`)
 			setOnlinePlayersLeague(payload)
 		})
 	
+		// Existing user left the league (not used)
+		// use case: tdb
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.LEAGUE.USER_OFFLINE, (payload) => {
 			console.log(`[Socket] user left league room "${socket.league_room}". users online: ${payload}`)
 			setOnlinePlayersLeague(payload)
 		})
 
+		// Existing user deleted from the League (not used)
+		// use case: force reload league data to not see the user in Team page
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.LEAGUE.USER_DELETED, (payload) => {
 			console.log(`[Socket] user deleted from league. users online: ${payload}`)
 			//TODO: fetch league data again
 			setOnlinePlayersLeague(payload)
 		})
 
-		// Market events
+		// ------- Market events -------
+		// Admin open the market (used)
+		// Use case: show market as open
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.OPEN, (payload) => {
 			console.log(`[Socket] open market room "${socket.market_room}". users online: ${payload}`)
 			setMarketOpen(true)
 		})
 
+		// Admin close the market (used) - fix bug on server
+		// Use case: show market as closed
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.CLOSE, (payload) => {
 			console.log(`[Socket] close market room "${socket.market_room}". users online: ${payload}`)
 			setMarketOpen(false)
+			setMarketStart(false)
 		})
 
+		// Existing user left the Market (not implemented)
+		// use case: show online users in market waiting room
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.USER_OFFLINE, (payload) => {
 			console.log(`[Socket] user left room "${socket.market_room}". users online: ${payload}`)
 			setOnlinePlayersMarket(payload)
 		})
 
+		// Existing user joined the Market (used)
+		// use case: show online users in market waiting room
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.USER_ONLINE, (payload) => {
 			console.log(`[Socket] user join room "${socket.market_room}". users online: ${payload}`)
 			setOnlinePlayersMarket(payload)
 		})
 
+		// Admin start the Auction (not used)
+		// use case: set the market as started
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.START, (payload) => {
 			console.log(`[Socket] market room "${socket.market_room}"is started. users online: ${payload}`)
 			setMarketStart(true)
 		})
 
+		// Event receiver immediately after SERVER.MARKET.START with player name's turn (not used)
+		// use case: render market open depending on the player's turn
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.SEARCH, (payload) => {
 			console.log(`[Socket] market room "${socket.market_room}". turn: ${payload.turn}`)
 			setMarketTurnUser(payload)
 		})
 
+		// User placed a bet on the selected player (not used)
+		// Use case: all other users will see Market Open Player Selected page
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.FOOTBALL_PLAYER_SELECTED, (payload) => {
 			console.log(`[Socket] market room "${socket.market_room}". ${payload.user} selected football_player_id: ${payload.football_player_id}, bet: ${payload.bet}`)
 		})
 
+		// Event recevied 3 seconds after SERVER.MARKET.FOOTBALL_PLAYER_SELECTED (not used)
+		// Use case: show Market Open Auction page
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.BET, (payload) => {
 			console.log(`[Socket] market room "${socket.market_room}". ${payload.user} bet on football_player_id: ${payload.football_player_id}, bet: ${payload.bet}`)
 		})
 
+		// Admin paused the Market (not used)
+		// Use case: tbd
 		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.PAUSE, () => {
 			console.log(`[Socket] market room "${socket.market_room}" is paused`)
 			setMarketPause(true)
 			// TODO: handle resume/start
-		})
-
-		ioClient.on(SocketManager.EVENT_TYPE.SERVER.MARKET.CLOSE, () => {
-			console.log(`[Socket] market room "${socket.market_room}" is closed`)
-			setMarketStart(false)
 		})
 
 		return () => {
